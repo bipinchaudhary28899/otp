ee6dcfc663b32d4967b71a776ad22d441de40350
bipinchaudhary28899 <45562225+bipinchaudhary28899@users.noreply.github.com>
2020-10-04 09:59:44 +0530

    Update index.js
    changed the accountSID,authTOKEN and service ID with a valid one

--- otp/index.js
+++ otp/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = 8080;
+const port = 8000;
 const config = require("./config");
 const path = require("path");
 
@@ -14,14 +14,14 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const accountSid = "AC4c2caab6cbde721ca09d309b08478075";
-const authToken = "8138ed6f045548f4556d781fc56226ec";
+const authToken = "086f8de53dcb8c44abe2d21eb2a87376";
 const client = require("twilio")(accountSid, authToken);
 
 // for otp createion..
 
 app.post("/login", (req, res) => {
   client.verify
-    .services("	VA6dba972120400fb646b68e05b3470fdb")
+    .services("VA6dba972120400fb646b68e05b3470fdb")
     .verifications.create({ to: `+91${req.body.mobile}`, channel: "sms" })
     .then((verification) => {
       console.log(verification);
@@ -34,7 +34,7 @@ app.post("/login", (req, res) => {
 // for verification...
 app.post("/verify", (req, res) => {
   client.verify
-    .services("	VA6dba972120400fb646b68e05b3470fdb")
+    .services("VA6dba972120400fb646b68e05b3470fdb")
     .verificationChecks.create({
       to: `+91${req.body.mobile}`,
       code: req.body.OTP,
